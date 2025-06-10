36d6611886c0cf269cb0b2a44e23e579461669eb
Naswa Gyna Sahira <naswagyna@gmail.com>
2025-06-10 18:31:25 +0700

    konfigurasi model mahasiswa

--- TUBESWAD_KONSELING/app/Models/Mahasiswa.php
+++ TUBESWAD_KONSELING/app/Models/Mahasiswa.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mahasiswa extends Model
 {
-    //
+    protected $fillable = [
+        'nama',
+        'nim',
+        'jurusan',
+        'fakultas',
+    ];
 }
