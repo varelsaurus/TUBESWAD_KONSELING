86b2302d26182d8663c6206feba421b4ac321c2e
kaylandine <kaylaandiniannisa@gmail.com>
2025-06-10 21:42:58 +0700

    tambah route konselor

--- TUBESWAD_KONSELING/routes/web.php
+++ TUBESWAD_KONSELING/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KonselorController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('konselor', KonselorController::class);
