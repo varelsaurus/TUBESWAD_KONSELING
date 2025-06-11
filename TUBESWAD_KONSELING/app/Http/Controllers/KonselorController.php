840e4cf1ebdc842723d9efcc032c5756340d2b0c
kaylandine <kaylaandiniannisa@gmail.com>
2025-06-11 15:05:19 +0700

    tambah update  konselor

--- TUBESWAD_KONSELING/app/Http/Controllers/KonselorController.php
+++ TUBESWAD_KONSELING/app/Http/Controllers/KonselorController.php
@@ -27,6 +27,17 @@ class KonselorController extends Controller
     public function edit(Konselor $konselor) {
         return view('konselor.edit', compact('konselor'));
     }
+    public function update(Request $request, Konselor $konselor) {
+        $request->validate([
+            'nama' => 'required',
+            'spesialisasi' => 'required',
+            'kontak' => 'required',
+        ]);
+
+        $konselor->update($request->all());
+        return redirect()->route('konselor.index')->with('success', 'Data berhasil diperbarui.');
+    }
+
 
 
 
