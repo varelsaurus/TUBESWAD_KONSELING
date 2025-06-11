8e4e4e0fe0a8264a6d39f94ad1b925867e587061
fedayeen-fairyzia <revafedayeen@gmail.com>
2025-06-11 18:22:30 +0700

    Membuat routing website

--- TUBESWAD_KONSELING/routes/web.php
+++ TUBESWAD_KONSELING/routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\KeluhanController;
+use App\Http\Controllers\MahasiswaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+Route::get('/keluhan', [KeluhanController::class, 'index'])->name('keluhan.index');
+Route::get('/keluhan/create', [KeluhanController::class, 'create'])->name('keluhan.create');
+Route::post('/keluhan', [KeluhanController::class, 'store'])->name('keluhan.store');
+Route::get('/keluhan/{id}', [KeluhanController::class, 'show'])->name('keluhan.show');
+Route::get('/keluhan/{id}/edit', [KeluhanController::class, 'edit'])->name('keluhan.edit');
+
+Route::put('/keluhan/{id}', [KeluhanController::class, 'update'])->name('keluhan.update');
+Route::delete('/keluhan/{id}', [KeluhanController::class, 'destroy'])->name('keluhan.destroy');
+    
+// });
