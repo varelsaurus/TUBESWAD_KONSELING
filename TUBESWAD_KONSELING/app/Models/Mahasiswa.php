4ea340f0fc065be1811ff0f70099092bdaacf1ac
Naswa Gyna Sahira <naswagyna@gmail.com>
2025-06-12 14:59:15 +0700

    Update mahasiswa.php

--- TUBESWAD_KONSELING/app/Models/Mahasiswa.php
+++ TUBESWAD_KONSELING/app/Models/Mahasiswa.php
@@ -6,10 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mahasiswa extends Model
 {
+<<<<<<< Updated upstream
+
+=======
+>>>>>>> Stashed changes
     protected $fillable = [
         'nama',
         'nim',
         'jurusan',
         'fakultas',
     ];
+<<<<<<< Updated upstream
+
+
+    public function keluhan()
+    {
+        return $this->hasMany(Keluhan::class, 'mahasiswaId', 'mahasiswaId');
+    }
+=======
+>>>>>>> Stashed changes
 }
