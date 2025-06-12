b74eb04981a7dedd276ed84537c50640d3641e9b
rohadaturrofidah <rofirohadaturrofidah@gmail.com>
2025-06-12 21:12:06 +0700

    terbaru

--- TUBESWAD_KONSELING/routes/web.php
+++ TUBESWAD_KONSELING/routes/web.php
@@ -3,12 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/login'); 
+    return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
