cbbb1b97c15666550948923faf3d852c4de3c0db
Naswa Gyna Sahira <naswagyna@gmail.com>
2025-06-12 22:12:21 +0700

    fix model

--- TUBESWAD_KONSELING/app/Models/Mahasiswa.php
+++ TUBESWAD_KONSELING/app/Models/Mahasiswa.php
@@ -6,18 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 use Laravel\Sanctum\HasApiTokens;
 use \Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class mahasiswa extends Model
+class Mahasiswa extends Model
 {
-
     protected $fillable = [
         'nama',
         'nim',
         'fakultas',
     ];
-
-
-    public function keluhan()
-    {
-        return $this->hasMany(Keluhan::class, 'mahasiswaId', 'mahasiswaId');
-    }
 }
