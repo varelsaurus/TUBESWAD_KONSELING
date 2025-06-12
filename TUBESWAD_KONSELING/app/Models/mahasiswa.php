73e767421c54f41325db954ca29224caa32c1460
Naswa Gyna Sahira <naswagyna@gmail.com>
2025-06-12 14:57:51 +0700

    model mahasiswa

--- TUBESWAD_KONSELING/app/Models/mahasiswa.php
+++ TUBESWAD_KONSELING/app/Models/mahasiswa.php
@@ -8,21 +8,15 @@ use \Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class mahasiswa extends Model
 {
-    use HasApiTokens, HasFactory;
 
-    //
-    protected $table = 'mahasiswa';
-    protected $primaryKey = 'mahasiswaId';
     protected $fillable = [
-        'mahasiswaId',
         'nama',
         'nim',
-        'kontak',
+        'jurusan',
+        'fakultas',
     ];
-    public $timestamps = true;
-    protected $casts = [
-        'mahasiswaId' => 'integer',
-    ];
+
+
     public function keluhan()
     {
         return $this->hasMany(Keluhan::class, 'mahasiswaId', 'mahasiswaId');
